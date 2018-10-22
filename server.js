e6505842be11208b91c718d728fad6b69a4b6652
unknown <gustavogonz3393@gmail.com>
2018-10-21 21:24:18 -0500

    heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/thisIsColossalPopulated", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/thisIsColossalPopulated", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/thisIsColossalPopulated";
 
 mongoose.connect(MONGODB_URI);
